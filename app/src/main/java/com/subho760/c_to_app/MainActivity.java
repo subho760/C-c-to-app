957c8c5808c0e1464e4da03efc66bf796f7fc8c2
subho760 <nightshadow5g@gmail.com>
2026-06-17 18:37:58 +0530

    Create MainActivity.java

---
+++ app/src/main/java/com/subho760/c_to_app/MainActivity.java
@@ -0,0 +1,6 @@
+package com.night.background;
+
+// This is a placeholder file to force Gradle to build the APK container.
+// The AndroidManifest tells the system to boot straight into your C++ instead!
+public class MainActivity {
+}
