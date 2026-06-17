14fbde291e372218c26df53358f721e8ab1e84d9
subho760 <nightshadow5g@gmail.com>
2026-06-17 17:36:49 +0530

    Update my_logic.h

--- native-lib/src/main/cpp/my_logic.h
+++ native-lib/src/main/cpp/my_logic.h
@@ -1,1 +1,10 @@
 
+#ifndef MY_LOGIC_H
+#define MY_LOGIC_H
+
+// Place your custom C/C++ calculations or core app logic functions here
+inline int calculateSomething(int a, int b) {
+    return a + b;
+}
+
+#endif // MY_LOGIC_H
