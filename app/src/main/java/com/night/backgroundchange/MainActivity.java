2c3ac8bc8d24d29cab6c95833b08f4a111bc6883
subho760 <nightshadow5g@gmail.com>
2026-06-24 14:27:38 +0530

    Fix method signature for onCreate in MainActivity

--- app/src/main/java/com/night/backgroundchange/MainActivity.java
+++ app/src/main/java/com/night/backgroundchange/MainActivity.java
@@ -11,7 +11,7 @@ import java.util.Random;
 public class MainActivity extends Activity {
 
     @Override
-    protected void Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
