3cd52a09a6021782c12c30d8c767ff6e07591b03
subho760 <nightshadow5g@gmail.com>
2026-07-09 18:42:45 +0530

    Update Particle.java

--- app/src/main/java/com/night/backgroundchange/Particle.java
+++ app/src/main/java/com/night/backgroundchange/Particle.java
@@ -1,1 +1,20 @@
+package com.night.backgroundchange;
 
+import java.util.Random;
+
+public class Particle {
+    public float x, y, vx, vy, alpha = 1.0f;
+    private Random res = new Random();
+
+    public Particle(float x, float y) {
+        this.x = x; this.y = y;
+        this.vx = (res.nextFloat() - 0.5f) * 10f;
+        this.vy = (res.nextFloat() - 0.5f) * 10f;
+    }
+
+    public boolean update() {
+        x += vx; y += vy;
+        alpha -= 0.05f;
+        return alpha <= 0;
+    }
+}
