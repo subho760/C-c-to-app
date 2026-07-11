d27b447516e0fff5fca23130e6c55f74ed90e3b3
subho760 <nightshadow5g@gmail.com>
2026-07-11 11:46:46 +0530

    Refactor build.gradle.kts by removing plugin and task
    Removed Android application plugin and clean task.

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,10 +8,9 @@ buildscript {
     }
 }
 
-plugins {
-    id("com.android.application") version "8.2.2" apply false
-}
-
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
