c271c4a6cc8cae7f417ab9fc2b03a3ca66969073
subho760 <nightshadow5g@gmail.com>
2026-07-20 09:22:00 +0530

    Add setStyle method to paint class reference

--- app/src/main/cpp/ui_manager.cpp
+++ app/src/main/cpp/ui_manager.cpp
@@ -497,7 +497,8 @@ Java_com_night_backgroundchange_MainActivity_nativeRender(JNIEnv* env, jobject o
 
         jclass paintCls = gameUI.paintTextReference ? env->GetObjectClass(gameUI.paintTextReference) : nullptr;
         jmethodID setTextSize = paintCls ? env->GetMethodID(paintCls, "setTextSize", "(F)V") : nullptr;
-        jmethodID setColor = paintCls ? env->GetMethodID(paintCls, "setColor", "(I)V");
+        jmethodID setStyle = paintCls ? env->GetMethodID(paintCls, "setStyle", "(Landroid/graphics/Paint$Style;)V") : nullptr;
+        jmethodID setColor = paintCls ? env->GetMethodID(paintCls, "setColor", "(I)V") : nullptr;
         jmethodID measureText = paintCls ? env->GetMethodID(paintCls, "measureText", "(Ljava/lang/String;)F") : nullptr;
 
         if (!gameUI.isPausePopupActive) {
